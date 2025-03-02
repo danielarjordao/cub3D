a0dba0505b14ea7103bea95a4df79ba3fcfb7a91
danielarjordao <danielarjordao@hotmail.com>
2025-03-02 16:01:12 +0000

    Merge dani

--- cub3D/src/parse/prints.c
+++ cub3D/src/parse/prints.c
@@ -8,7 +8,6 @@ void	print_map(t_map *map)
 	if (!map)
 		return ;
 	printf("PRINTING MAP\n\n");
-	printf("PRINTING MAP\n\n");
 	if (map->no_texture)
 		printf("North texture: %s\n", map->no_texture);
 	if (map->so_texture)
@@ -18,35 +17,28 @@ void	print_map(t_map *map)
 	if (map->ea_texture)
 		printf("East texture: %s\n", map->ea_texture);
 	printf("\n");
-		printf("East texture: %s\n", map->ea_texture);
-	printf("\n");
 	i = 0;
 	while (i < 3)
 	{
-		printf("Floor color[%d]: %d\n", i, map->floor_color[i]);
 		printf("Floor color[%d]: %d\n", i, map->floor_color[i]);
 		i++;
 	}
 	printf("Floor color hex:%X\n", map->floor_color_hex);
 	printf("\n");
-	printf("Floor color hex:%X\n", map->floor_color_hex);
-	printf("\n");
+
 	i = 0;
 	while (i < 3)
 	{
 		printf("Ceiling color[%d]: %d\n", i, map->ceiling_color[i]);
-		printf("Ceiling color[%d]: %d\n", i, map->ceiling_color[i]);
 		i++;
 	}
-	printf("Ceiling color hex: %X\n", map->ceiling_color_hex);
-	printf("\n");
 	printf("Ceiling color hex: %X\n", map->ceiling_color_hex);
 	printf("\n");
 	if (map->map)
 	{
 		i = 0;
 		printf("MAP\n");
-		printf("MAP\n");
+
 		while (map->map[i])
 		{
 			j = 0;

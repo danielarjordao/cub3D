94638999c6f6f9d3eadde31cba413b03d84f3a6e
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2025-03-27 12:34:21 -0300

    Add player position initialization and improve direction handling

--- cub3D/src/utils/init_data2.c
+++ cub3D/src/utils/init_data2.c
@@ -4,8 +4,8 @@ void	init_player_direction(t_game *game)
 {
 	if (game->map->player_dir == 'N')
 	{
-		game->map->player_dir_x = (float) 0;
-		game->map->player_dir_y = (float) 1.0;
+		game->map->player_dir_x = 0.0;
+		game->map->player_dir_y = 1.0;
 	}
 	else if (game->map->player_dir == 'W')
 	{
@@ -24,10 +24,20 @@ void	init_player_direction(t_game *game)
 	}
 }
 
+void	init_player_position(t_game *game)
+{
+	game->map->player_x += 0.5;
+	game->map->player_y += 0.5;
+}
+
 void	init_raycasting(t_game *game)
 {
 	ft_printf(1, "		Initializing raycasting\n");
 	ft_printf(1, "			Setting player view direction, %c\n", game->map->player_dir);
 	init_player_direction(game);
 	printf("			x = %f, y = %f\n", game->map->player_dir_x, game->map->player_dir_y);
+	ft_printf(1, "			Setting player position\n");
+	init_player_position(game);
+	printf("			pos_x = %f\n" , game->map->player_dir_x);
+	printf("			pos_y = %f\n", game->map->player_dir_y);
 }

9b57dee151ae307ddb04190652d7b1f7957da808
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2025-04-08 19:16:03 -0300

    Implement xpm_to_image function for loading textures and validating dimensions

--- cub3D/src/utils/utils.c
+++ cub3D/src/utils/utils.c
@@ -38,6 +38,37 @@ bool	msg_error(t_error err)
 	return (false);
 }
 
+void	*xpm_to_image(t_game *game, char *filename)
+{
+	void	*ptr_img;
+	int		*ptr_width;
+	int		*ptr_heigth;
+
+	ptr_width = ft_calloc(1, sizeof(int));
+	if (!ptr_width)
+		destroy_free_exit_error(game, "ft_calloc failed");
+	ptr_heigth = ft_calloc(1, sizeof(int));
+	if (!ptr_width)
+	{
+		free(ptr_width);
+		destroy_free_exit_error(game, "ft_calloc failed");
+	}
+	ptr_img = mlx_xpm_file_to_image(game->mlx->connection, filename, \
+		ptr_width, ptr_heigth);
+	printf("largura da imagem %d e deveria ser %d\n", *ptr_width, TEXTURE_WIDTH);
+	printf("altuta da imagem %d e deveria ser %d\n", *ptr_heigth, TEXTURE_HEIGHT);
+	if (*ptr_width != TEXTURE_WIDTH || *ptr_heigth != TEXTURE_HEIGHT)
+	{
+		free (ptr_width);
+		free(ptr_heigth);
+		mlx_destroy_image(game->mlx, ptr_img);
+		destroy_free_exit_error(game, "The size of the img must be 256x256");
+	}
+	free (ptr_width);
+	free(ptr_heigth);
+	return (ptr_img);
+}
+
 int	close_game(t_game *game)
 {
 	printf("You left the game\n");

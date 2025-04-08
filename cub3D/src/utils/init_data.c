4307806ddbf5e7a7f3a7c4655c78e1c9d3019b6b
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2025-04-08 19:15:39 -0300

    Implement texture initialization and memory allocation for textures

--- cub3D/src/utils/init_data.c
+++ cub3D/src/utils/init_data.c
@@ -66,22 +66,47 @@ void	init_mlx(t_game *game)
 	//REVIEW -> memoria verificada ate esse ponto
 }
 
+/* void	alloc_textures(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		game->textures[i] = ft_calloc(1, sizeof(t_texture));
+		check_mem(game, &(game->mem_alloc.ptr_mem_list), \
+			game->textures[i], "ft_calloc failed");
+		i++;
+	}
+} */
+
 void	init_textures(t_game *game)
 {
 	ft_printf(1, "		Initializing textures\n");
-	(void)game;
-	//TODO - Criar imagems
-	 game->textures = xpm_to_image(game, "./images/bat2.xpm");
-	/*
-	if (game->img_player_l == NULL || game->img_player_r == NULL
-	|| game->img_grass == NULL || game->img_tree == NULL
-	|| game->img_collectible == NULL || game->img_player_d == NULL
-	|| game->img_player_u == NULL || game->img_dead1 == NULL
-	|| game->img_dead2 == NULL || game->img_enemy1 == NULL
-	|| game->img_enemy2 == NULL || game->img_player_exit == NULL
-	|| game->img_exit == NULL)
-	destroy_free_exit_error(game, \
-	"mlx_xpm_file_to_image() in init_images failed"); */
+	// alloc_textures(game);
+	game->textures[NO].img = xpm_to_image(game, game->map->no_texture);
+	game->textures[NO].pixel_address = \
+		mlx_get_data_addr(game->textures[NO].img, \
+		&(game->textures[NO].bpp), &(game->textures[NO].size_line), \
+		&(game->textures[NO].endian));
+	game->textures[SO].img = xpm_to_image(game, game->map->so_texture);
+	game->textures[SO].pixel_address = \
+		mlx_get_data_addr(game->textures[SO].img, \
+		&(game->textures[SO].bpp), &(game->textures[SO].size_line), \
+		&(game->textures[SO].endian));
+	game->textures[EA].img = xpm_to_image(game, game->map->ea_texture);
+	game->textures[EA].pixel_address = \
+		mlx_get_data_addr(game->textures[EA].img, \
+		&(game->textures[EA].bpp), &(game->textures[EA].size_line), \
+		&(game->textures[EA].endian));
+	game->textures[WE].img = xpm_to_image(game, game->map->we_texture);
+	game->textures[WE].pixel_address = \
+		mlx_get_data_addr(game->textures[WE].img, \
+		&(game->textures[WE].bpp), &(game->textures[WE].size_line), \
+		&(game->textures[WE].endian));
+	if (game->textures[NO].img == NULL || game->textures[SO].img == NULL \
+		|| game->textures[EA].img == NULL || game->textures[WE].img == NULL)
+	destroy_free_exit_error(game, "mlx_xpm_file_to_image failed");
 }
 
 void	init_game(t_game *game)
